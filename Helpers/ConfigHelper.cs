99a48cf1490c571d77dd3c0b9f30de5b7ca2a719
Mike Williams (UK) <mike@doublewide.co.uk>
2024-11-08 08:16:21 +0000

    Add dump of json

---
+++ Helpers/ConfigHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace OvoData.Helpers;
+
+public static class ConfigHelper
+{
+    public static bool GetBoolean(IConfigurationRoot config, string key, bool defaultValue)
+    {
+        var result = defaultValue;
+
+        if (config != null)
+        {
+            try
+            {
+                var value = config[key];
+                if (bool.TryParse(value, out result))
+                {
+                    return result;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        return result;
+    }
+}
--- Helpers/Logger.cs
+++ Helpers/Logger.cs
@@ -31,4 +31,18 @@ public static class Logger
 
         return fileName;
     }
+
+    public static void DumpJson(string responseType, string json)
+    {
+        var folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), Constants.ApplicationName);
+
+        if (!Directory.Exists(Path.Combine(folder, "Dump")))
+        {
+            Directory.CreateDirectory(Path.Combine(folder, "Dump"));
+        }
+
+        var fileName = Path.Combine(folder, "Dump", $"{DateTime.Now:yyyy-MM-dd} {responseType}.json");
+
+        File.WriteAllText(fileName, json);
+    }
 }
